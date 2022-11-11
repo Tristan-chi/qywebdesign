9dd2312f292d59577470d602e44b26161377e1a3
Tristan <chiyunzhang4@gmail.com>
2022-11-11 15:48:59 +0800

    test

---
+++ src/main/resources/static/js/about.ce505faa.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkwebdesignfont"]=self["webpackChunkwebdesignfont"]||[]).push([[443],{7381:function(n,e,t){t.r(e),t.d(e,{default:function(){return b}});var s=t(3396);const u={class:"about"},a=(0,s._)("h1",null,"This is an about page",-1),r=[a];function i(n,e){return(0,s.wg)(),(0,s.iD)("div",u,r)}var c=t(89);const o={},f=(0,c.Z)(o,[["render",i]]);var b=f}}]);
+//# sourceMappingURL=about.ce505faa.js.map
