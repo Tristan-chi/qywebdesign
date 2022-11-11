11d6805d130d51176764adab565c9d9fb88473a4
Tristan <chiyunzhang4@gmail.com>
2022-11-11 15:28:36 +0800

    test

---
+++ src/main/java/com/qylab/webdesign/WebdesignApplication.java
@@ -0,0 +1,13 @@
+package com.qylab.webdesign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebdesignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebdesignApplication.class, args);
+    }
+
+}
